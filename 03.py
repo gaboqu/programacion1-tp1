5c20b4efd36df172c0b71c7c90960067774e4f1f
gaboqu <gabrielquiroga.e@gmail.com>
2024-04-14 21:05:13 -0300

    Agregados archivos del Trabajo Práctico Nº1

---
+++ 03.py
@@ -0,0 +1,3 @@
+nombre = input("Por favor, introduce tu nombre: ")
+apellido = input("Por favor, introduce tu apellido: ")
+print("Hola", nombre, apellido)
---
+++ 04.py
@@ -0,0 +1,4 @@
+n1 = int(input("Por favor, introduce un numero: "))
+n2 = int(input("Por favor, introduce otro numero: "))
+resultado= n1 + n2
+print ("el resultado es", resultado)
